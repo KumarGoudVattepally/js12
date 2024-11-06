bba3d9838ab8fe32276bb6495769aeaa8c840c4a
Kumar Goud Vattepally <kumargoudvattepally3@gmail.com>
2024-11-06 13:05:55 +0530

    js12

---
+++ index.js
@@ -0,0 +1,19 @@
+function jio(x) {
+  if (x > 0 && x % 2 == 0) {
+    console.log("even no");
+  } else if (x > 0 && x % 2 != 0) {
+    console.log("odd no");
+  } else {
+    console.log("invaild no");
+  }
+}
+jio(-6);//invaild no
+
+function jio1(y){
+    
+
+}
+    
+
+    
+
